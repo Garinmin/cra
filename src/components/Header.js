27d80dbad84b2905cd97698e4582c2846f836e42
Garinmin <garinmin@gmail.com>
2021-05-02 10:01:31 -0700

    lab01

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+function Header () {
+  return (
+    <h1> Horned-beast </h1>
+  )};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Main() {
+
+  return (
+    <>
+      <HornedBeasts/>
+    </>
+  );
+}
+
+function HornedBeasts () {
+  return (
+    <>
+      <h2> Name </h2>
+      <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg" alt="" width= "70%"/>
+      <p>Description</p>
+    </>
+  );
+}
+
+export default Main;
+
