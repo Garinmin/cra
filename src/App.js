c003ffc25d4605f5ae66bec50cc4c4cad32385e8
Garinmin <garinmin@gmail.com>
2021-05-02 12:42:03 -0700

    lab02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import React from 'react';
+import Header from './components/Header.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Container fluid>
+          <Header  />
+          <Main />
+          <Footer />
+        </Container>
+      );
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Item from './data.json';
+import { Component } from 'react';
+import { Container, CardColumns, Card, CardDeck } from 'react-bootstrap'
+
+
+
+function Main() {
+
+  return (
+    <Container fluid>
+      <CardColumns>
+        {Item.map(item => (
+          <HornedBeasts
+            title={item.title}
+            description={item.description}
+            imgUrl={item.image_url}
+          />
+        ))}
+      </CardColumns>
+    </Container>
+  );
+}
+
+class HornedBeasts extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+    }
+  }
+
+  addFavorite = () => {
+    this.setState({ likes: this.state.likes + 1 });
+  }
+
+  render() {
+
+    return (
+        <Card
+          bg="dark"
+          text="light"
+          onClick={this.addFavorite}
+        >
+          <Card.Img 
+            variant="top"
+            src={this.props.imgUrl} 
+            
+          />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              😍 = {this.state.likes}
+            </Card.Text>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.Body>  
+        </Card>
+    );
+  }
+}
+
+export default Main;
